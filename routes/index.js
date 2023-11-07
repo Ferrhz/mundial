aaa5f3dfcf71a00cf5cd531fa6c3c84de719ab0a
Fernando Tadeu Silva de Oliveira <Fernando.Tadeu@mundialcalcados.local>
2023-11-07 08:41:34 -0300

    ajuste

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const indexController = require('../controller/indexController')
 
-router.get('/home', indexController.index)
+router.get('/', indexController.index)
 
 module.exports = router;
